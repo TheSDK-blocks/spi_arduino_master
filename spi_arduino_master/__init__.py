590db637c5bdb617a6cd4240d4e4df31ec0955e4
Spelman Andrei <spelmaa2@t40309-l006.org.aalto.fi>
2023-12-13 16:01:22 +0200

    New structure for SPI

--- spi_arduino_master/__init__.py
+++ spi_arduino_master/__init__.py
@@ -63,7 +63,7 @@ class spi_arduino_master():
         #self.IOS.Members['control_write']= IO() 
         #This is a placeholder, file is created by controller
         #_=rtl_iofile(self,name='control_write', dir='in', iotype='event', ionames=['reset', 'initdone', 'io_cs', 'io_mosi', 'io_sclk'])
-        self.arduino = Serial('/dev/ttyACM0',baudrate=115200)
+        self.arduino = Serial('/dev/ttyACM0',baudrate=9600)
         print(self.arduino.name)
         time.sleep(0.1) 
         if len(arg)>=1:
@@ -82,30 +82,176 @@ class spi_arduino_master():
             d=self.arduino.write(bytes(x, 'utf-8'))
             #d=arduino.write(x)
             #time.sleep(0.001)
-            data = self.arduino.read(size=1)
+            #data = self.arduino.read(size=1)
+            data='0'
             return data
 
-    def main(self):
+
+
+    def writeToMem(self,conf):
         
-        
-        const_time=False
-        time_step=1
-        time_step_fact=10**(-1) 
-                
-        #print(arduino.readline())
-        #time.sleep(0.1)
-        if self.read_from=='file':
-            first_line=True
-            prev_time=0
-            with open('in1.txt') as f, open('out1.txt', 'w') as f2:
-                start_time=time.time_ns()
+        #pdb.set_trace()
+        conf_len=len(conf)
+        bytes_num=int(np.ceil(len(conf)/8))
+        conf_array=[0]*(bytes_num)
+        #conf_array[0]='w'
+        z=''.join(map(str,np.zeros(bytes_num*8-conf_len).astype(int)))
+        conf= z+conf
+        conf=conf[::-1]
+        #d=self.arduino.write(bytes('w', 'utf-8'))
+        for i in range(0,bytes_num):
+            #pdb.set_trace()
+            test=conf[8*i:8*(i+1)]
+            data=str(int(conf[8*i:8*(i+1)],2))
+            print(test)
+            print(data)
+            conf_array[i]=int(data)
+            #conf_array[i]=''.join([chr(int(test,2))])
+
+        #pdb.set_trace()
+        w=bytes('w', 'utf-8')
+        self.arduino.write(w)
+        self.arduino.flush()
+        for x in conf_array:
             
-                for line in f:
+            a=self.arduino.write(x.to_bytes(1,'big'))
+            self.arduino.flush()
+            #self.arduino.flush()
+            time.sleep(0.1)
+            #self.arduino.flush()
+        #    self.arduino.write(x.encode())  # Send the 20-bit string to Arduino
+        #    self.arduino.flush()
+        #    pdb.set_trace()
+        #self.arduino.write(conf_array)  # Send the 20-bit string to Arduino
+        self.arduino.flush()
+        #pdb.set_trace()
+        time.sleep(1)
+        #pdb.set_trace()
+        print(self.arduino.in_waiting)
+        if self.arduino.in_waiting==0:
+            print("For some reason sometimes writing fails, try again. Sorry")
+            exit()
+
+        while 1:
+            if self.arduino.in_waiting>0:
+                print(self.arduino.read(1))
+            else:
+                break
+        #d=self.arduino.write(bytes(conf_array))
+        #d=self.arduino.write(bytes(conf_array, 'utf-8'))
+
+        a=5
+
+    def pushConf(self,conf):
+        conf_len=len(conf)
+        bytes_num=int(np.ceil(len(conf)/8))
+        mon_array=np.zeros(bytes_num)
+        n=bytes('n', 'utf-8')
+        self.arduino.reset_input_buffer()
+        self.arduino.write(b'p')
+        self.arduino.flush()
+        #pdb.set_trace()
+
+        while 1:
+            if self.arduino.in_waiting>1:
+                #pdb.set_trace()
+                test=self.arduino.read(1)
+                print(test)
+                if test.decode()=='a':
+                    #self.arduino.read(2)
+                    break
+
+                 
+
+        i=0
+        while 1:
+            if self.arduino.in_waiting>0:
+                val=self.arduino.read(1)
+                mon_array[i]=int.from_bytes(val,'big')
+                self.arduino.write(n)
+                i=i+1
+
+            if i==bytes_num:
+                break
+
+        #x= self.arduino.read(size=3)
+        #y=''.join(format(byte,'08b') for byte in x)
+
+
+        #d=self.arduino.write(bytes('p', 'utf-8'))
+
+        #mon_array = self.arduino.read(size=bytes_num)
+        monitor=''
+        for x in mon_array:
+            monitor=monitor+np.binary_repr(int(x),8)
+        monitor=monitor[::-1]
+        self.arduino.reset_input_buffer()
+        pdb.set_trace()
+        a=5
+        return monitor
+
+    def main(self):
+        structure=2 # 1, 2
+        if structure ==1: 
+            const_time=False
+            time_step=1
+            time_step_fact=10**(-1)                 
+            #pdb.set_trace() 
+            #print(arduino.readline())
+            #time.sleep(0.1)
+            if self.read_from=='file':
+                first_line=True
+                prev_time=0
+                with open('in1.txt') as f, open('out1.txt', 'w') as f2:
+                    start_time=time.time_ns()
+                
+                    for line in f:
+                        pdb.set_trace()
+                        if const_time==False:
+                            time_step=int(line.strip().split(' ')[0])-prev_time
+                        prev_time=int(line.strip().split(' ')[0])
+                        data=str(int(("".join(line.strip().split(' ')[1:4])),2))
+                        time.sleep(time_step*time_step_fact)
+                        #num = input("Enter a number: ") # Taking input from user
+                        #if int(num) ==int(5):
+                        #    
+                        time_r1=time.time_ns()
+                        value = self.write_read(data).strip()
+                        time_r2=time.time_ns()
+                        print('Writetime='+str((time_r2-time_r1)/ (10 ** 9)))
+                        print(value) # printing the valueerial
+                        f2.write(str(prev_time)+' '+str(int(value))+'\n')
+                    stoptime=time.time_ns()
+                    print((stoptime-start_time)/ (10 ** 9))
+            elif self.read_from=='IOs':
+                #t=self.IOS.Members['time'].Data
+                #cs=self.IOS.Members['cs'].Data
+                #sclk=self.IOS.Members['sclk'].Data
+                #mosi=self.IOS.Members['mosi'].Data
+                #miso=np.zeros(len(t))
+                #pdb.set_trace() 
+                t=np.arange(22).reshape(-1,1)
+                cs=np.zeros_like(t)
+                cs[len(t)-1]=1
+                sclk=np.zeros_like(t)
+                sclk[np.arange(len(t))[1::2]]=1
+                mosi=np.zeros_like(t)
+                mosi[np.array([4,5,6,7,12,13,14,15,19,20,21])]=1
+                miso=np.zeros(len(t))
+
+                start_time=time.time_ns()
+                prev_time=0
+                data=(np.hstack((sclk,mosi,cs)))# .astype(str)
+                data_in=data.dot(2**np.arange(3)[::-1]).astype(str)
+                #data_in=np.char.add(data[:,0],data[:,1])
+                #data_in=np.char.add(data_in,data[:,2])
+                for i in range(len(data_in)):
+
                     #pdb.set_trace()
                     if const_time==False:
-                        time_step=int(line.strip().split(' ')[0])-prev_time
-                    prev_time=int(line.strip().split(' ')[0])
-                    data=str(int(("".join(line.strip().split(' ')[1:4])),2))
+                        time_step=t[i][0]-prev_time
+                    prev_time=t[i][0]
+                    data=data_in[i]
                     time.sleep(time_step*time_step_fact)
                     #num = input("Enter a number: ") # Taking input from user
                     #if int(num) ==int(5):
@@ -115,54 +261,33 @@ class spi_arduino_master():
                     time_r2=time.time_ns()
                     print('Writetime='+str((time_r2-time_r1)/ (10 ** 9)))
                     print(value) # printing the valueerial
-                    f2.write(str(prev_time)+' '+str(int(value))+'\n')
+                    miso[i]=value
                 stoptime=time.time_ns()
                 print((stoptime-start_time)/ (10 ** 9))
-        elif self.read_from=='IOs':
-            #t=self.IOS.Members['time'].Data
-            #cs=self.IOS.Members['cs'].Data
-            #sclk=self.IOS.Members['sclk'].Data
-            #mosi=self.IOS.Members['mosi'].Data
-            #miso=np.zeros(len(t))
-            #pdb.set_trace() 
-            t=np.arange(22).reshape(-1,1)
-            cs=np.zeros_like(t)
-            cs[len(t)-1]=1
-            sclk=np.zeros_like(t)
-            sclk[np.arange(len(t))[1::2]]=1
-            mosi=np.zeros_like(t)
-            mosi[np.array([4,5,6,7,12,13,14,15,19,20,21])]=1
-            miso=np.zeros(len(t))
 
-            start_time=time.time_ns()
-            prev_time=0
-            data=(np.hstack((sclk,mosi,cs)))# .astype(str)
-            data_in=data.dot(2**np.arange(3)[::-1]).astype(str)
-            #data_in=np.char.add(data[:,0],data[:,1])
-            #data_in=np.char.add(data_in,data[:,2])
-            for i in range(len(data_in)):
-
-                #pdb.set_trace()
-                if const_time==False:
-                    time_step=t[i][0]-prev_time
-                prev_time=t[i][0]
-                data=data_in[i]
-                time.sleep(time_step*time_step_fact)
-                #num = input("Enter a number: ") # Taking input from user
-                #if int(num) ==int(5):
-                #    
-                time_r1=time.time_ns()
-                value = self.write_read(data).strip()
-                time_r2=time.time_ns()
-                print('Writetime='+str((time_r2-time_r1)/ (10 ** 9)))
-                print(value) # printing the valueerial
-                miso[i]=value
-            stoptime=time.time_ns()
-            print((stoptime-start_time)/ (10 ** 9))
-
+                pdb.set_trace()
+                #self.IOS.Members['miso'].Data=miso
+                a=5
+        elif structure==2:
             #pdb.set_trace()
-            #self.IOS.Members['miso'].Data=miso
-            #a=5
+            conf_len=20
+            bytes_num=int(np.ceil(conf_len/8))
+            conf_array=np.zeros(bytes_num)
+            mon_array=np.zeros(bytes_num)
+            conf=np.zeros(conf_len)
+            #conf[::2]=1
+            conf=''.join(map(str,conf.astype(int)))
+            conf='101000000000000000011111111'
+            #z=''.join(map(str,np.zeros(bytes_num*8-conf_len).astype(int)))
+            #conf= z+conf
+            self.writeToMem(conf)
+            time.sleep(1)
+            monitor=self.pushConf(conf)
+            print(monitor[len(monitor)-len(conf):]==conf)
+
+
+
+            a=5
     def run(self,*arg):
         if len(arg)>0:
             self.par=True      #flag for parallel processing
--- spi_master/spi_master.ino
+++ spi_master/spi_master.ino
@@ -2,7 +2,8 @@
 //int y;
 //char in_mem;//[3];
 void setup() {
- Serial.begin(115200);
+ 
+ Serial.begin(9600);
  //Serial.setTimeout(1);
  
   
@@ -24,34 +25,148 @@ void setup() {
   
   
 }
+// Implementation compatible with SDK spi_slave controller generated files
+//void loop() {
+// while (!Serial.available());
+// //int x = Serial.readBytes(in_mem,1);
+// //long int t1=micros(); 
+// char x1=Serial.read();
+// //char x2=Serial.read();
+// //char x3=Serial.read();
+// //byte x=()in_mem;
+// //Serial.println(bitRead(x1,0));
+// //Serial.println(bitRead(x1,1));
+// //Serial.println(bitRead(x1,2));
+// //Serial.println(int(in_mem));
+// //Serial.println(int(in_mem[1]));
+// //Serial.println(int(in_mem[2]));
+// //Serial.println("\n");
+// //Serial.println(*in_mem);
+// 
+// digitalWrite(SS, bitRead(x1,0));
+// digitalWrite(SCK, bitRead(x1,2));
+// digitalWrite(MOSI, bitRead(x1,1));
+// Serial.print(digitalRead(MISO));
+// //long int t2=micros();
+// //Sierial.println(t2-t1);
+// //char t='123';
+// //Serial.println(t);
+// //Serial.println("\n");
+// //int y=in_mem[0].toInt();
+// //Serial.println(y);
+// //Serial.println(x + 1);
+//	
+//}
+
+
+const int freq=1;
+ const  unsigned long step_time=((1/freq)/2)*pow(10,6);
+ const float conf_len=27;
+ const int bytes_num=ceil(conf_len/8);
+ const int leftover=8-(bytes_num*8-conf_len);
+ byte conf_array[bytes_num];
+ byte mon_array[bytes_num];
+int currentState =0;
+
+// Implementation for arbitrary SPI master 
 void loop() {
- while (!Serial.available());
- //int x = Serial.readBytes(in_mem,1);
- //long int t1=micros(); 
- char x1=Serial.read();
- //char x2=Serial.read();
- //char x3=Serial.read();
- //byte x=()in_mem;
- //Serial.println(bitRead(x1,0));
- //Serial.println(bitRead(x1,1));
- //Serial.println(bitRead(x1,2));
- //Serial.println(int(in_mem));
- //Serial.println(int(in_mem[1]));
- //Serial.println(int(in_mem[2]));
- //Serial.println("\n");
- //Serial.println(*in_mem);
- 
- digitalWrite(SS, bitRead(x1,0));
- digitalWrite(SCK, bitRead(x1,2));
- digitalWrite(MOSI, bitRead(x1,1));
- Serial.print(digitalRead(MISO));
- //long int t2=micros();
- //Sierial.println(t2-t1);
- //char t='123';
- //Serial.println(t);
- //Serial.println("\n");
- //int y=in_mem[0].toInt();
- //Serial.println(y);
- //Serial.println(x + 1);
-	
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    if (command == 'w') {
+      currentState = 1;
+    } else if (command == 'p') {
+      currentState = 2;
+    } else {
+      currentState = 0; // Return to the "doing nothing" state if an unknown command is received
+    }
+  }
+
+  // State machine
+  switch (currentState) {
+    case 0: // Doing nothing
+      break;
+    case 1: // "w" state
+      //Serial.println(char('w'));
+      //Serial.println(conf_len);
+      //Serial.println(bytes_num);
+      writeStringToArduino();
+      break;
+    case 2: // "p" state
+      //Serial.println(char('p'));
+      readWriteData();
+      break;
+    default:
+      break;
+  }
 }
+
+void writeStringToArduino() {
+  
+    //Serial.println(char('z'));
+    while(Serial.available() <1){}
+    
+    for (int i = 0; i < bytes_num; i++) {
+      //Serial.println(i);
+      while(Serial.available() <1){}
+      if (Serial.available() >0) {
+        //Serial.println('y');
+        conf_array[i] = byte(Serial.read());
+        Serial.read();
+        delay(100);
+      }
+    currentState = 0; // Return to the "doing nothing" state
+    Serial.write(conf_array[0]);
+    Serial.write(conf_array[1]);
+    Serial.write(conf_array[2]);
+    Serial.write(conf_array[3]);
+  }
+}
+
+void readWriteData() {
+  
+  for (int i = 0; i < bytes_num; i++) {
+    //Serial.println(char('x'));
+    char currentChar = conf_array[i];
+    for (int bitIdx = 0; bitIdx < 8; bitIdx++) {
+      // Extract the current bit from the character
+      if (!(i == bytes_num-1 && bitIdx == leftover)) {
+        int currentBit = (currentChar >> bitIdx) & 0x01;
+        delayMicroseconds(step_time);
+        //delayMicroseconds(1000000);
+        
+        //delay(100);
+        //Serial.println(currentBit);
+        digitalWrite(MOSI, currentBit); 
+        //digitalWrite(MOSI, 1);
+        digitalWrite(SCK, 1);
+        //delay(10);
+        mon_array[i] |= (digitalRead(MISO) == HIGH) << bitIdx; 
+        delayMicroseconds(step_time);
+        //delayMicroseconds(1000000);
+        //delay(100);
+        digitalWrite(SCK, 0);
+        
+      }
+    }
+  }
+  digitalWrite(SCK, 0);
+  digitalWrite(MOSI, 0);
+  //mon_array[bytes_num] |= (digitalRead(MISO) == HIGH) << 7;
+  // Send array2 to Python
+  delay(1000);
+  Serial.write(char('a'));
+  for (int i = 0; i < bytes_num; i++) {
+    Serial.write(mon_array[i]);
+    delay(100);
+    while(Serial.available() <1){}
+    Serial.read();
+    //Serial.println(char('b'));
+  }
+  for (int i = 0; i < bytes_num; i++) {
+    Serial.write(conf_array[i]);
+    Serial.write(mon_array[i]);
+    delay(100);
+  }
+  currentState = 0; // Return to the "doing nothing" state
+}
+
